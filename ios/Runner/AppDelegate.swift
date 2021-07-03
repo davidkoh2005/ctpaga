176c5359f8986438ab23f47f837c30ca4fef9439
Robed Jules <robedjules@Mac-mini.local>
2021-07-03 19:15:38 -0400

    fix error

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,12 +8,13 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
+    FirebaseApp.configure()  
+
     GeneratedPluginRegistrant.register(with: self)
 
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }  
+    }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
